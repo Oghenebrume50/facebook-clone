204df0f2de258b7628735ed9f22dc0504f0051fa
TERVER AOSU <terveraosu@gmail.com>
2019-07-12 17:30:40 +0100

    Created models for likes, comments and friendings

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many :posts, foreign_key: :author_id
+         has_many :comments
+         has_many :likes
+     
+         has_many :friendings, foreign_key: :inviting_user_id
+         has_many :friendings, foreign_key: :invited_user_id
+     
+         has_many :invited_users, through: :friendings
+         has_many :inviting_users, through: :friendings             
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_161709) do
+ActiveRecord::Schema.define(version: 2019_07_12_161010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friendings", force: :cascade do |t|
+    t.boolean "status"
+    t.bigint "inviting_user_id"
+    t.bigint "invited_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invited_user_id"], name: "index_friendings_on_invited_user_id"
+    t.index ["inviting_user_id"], name: "index_friendings_on_inviting_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
@@ -36,5 +65,11 @@ ActiveRecord::Schema.define(version: 2019_07_10_161709) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "friendings", "users", column: "invited_user_id"
+  add_foreign_key "friendings", "users", column: "inviting_user_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users", column: "author_id"
 end
