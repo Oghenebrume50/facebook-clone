8d9c82a5a8080df41e8e11f574089c119dfabb91
Oghenebrume50 <raphlbrume@gmail.com>
2019-07-15 17:51:33 +0100

    setup all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'posts/index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
