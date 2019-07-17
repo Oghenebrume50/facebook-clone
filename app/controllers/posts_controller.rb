ee117c7c3a921550aa9b7eec307fa3ffd3979896
Oghenebrume50 <raphlbrume@gmail.com>
2019-07-17 11:07:50 +0100

    setup post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
-  def index
+
+  def new
+    @post = Post.new
   end
+
+  def create
+    @post = Post.new
+
+    if @post.save
+      redirect_to posts_index_path
+    else 
+      render 'new'
+    end
+
+  end
+
+  def index
+    @posts = Post.all
+  end
+
 end
