0028fb9c74d5311c251b6638b09be003d078780b
TERVER AOSU <terveraosu@gmail.com>
2019-07-27 22:18:33 +0100

    made the tests for the create new post to pass

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -15,21 +15,25 @@ RSpec.describe PostsController, type: :controller  do
 
     describe "create new post" do
       before(:each) do
-        @user = FactoryGirl.create(:user)
       end
-      # context "when post creation success" do
-      #   it "creates post" do
-      #     post :create, user_id: :user, post: { content: "Dummy name", user_id: :user}
-      #     expect(response).to redirect_to(:user)
-      #   end  
-      # end
-
-      # context "when post creation fails" do
-      #   it "creates post" do
-      #     post :create, user_id: :user, post: { content: "Dummy name", user_id: :user}
-      #     expect(response).to redirect_to(:user)
-      #   end  
-      # end      
-    end    
+      context "when post creation success" do
+        it "creates post with content" do
+          params = {
+            content: "hello this is a test post",            
+          }
+          expect { post(:create, params: { user_id: :user, post: params }) }.to change(Post, :count).by(1)
+        end  
+      end
+  
+      context "when post creation fails" do
+        it "creates post without content" do
+          params = {
+            content: nil            
+          }
+          expect { post(:create, params: { user_id: :user, post: params }) }.to_not change(Post, :count)
+        end  
+      end      
+    end      
+  
 end
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it "is invalid without content" do
-    user = User.create(
-      name:  "poster",
-      email:      "poster@example.com",
-      password:   "123456",      
-    )
-    new_post = user.posts.create(content: nil)
-    expect(new_post.errors[:content]).to include("can't be blank")
-  end
+  describe "create new post" do
+    before(:each) do
+      user = :user
+      @user = User.create(
+        name:  "poster",
+        email:      "poster@example.com",
+        password:   "123456",      
+      )
+    end
+    context "when post creation success" do
+    end
+
+    context "when post creation fails" do
+      it "is invalid without content" do
+        new_post = @user.posts.create(content: nil)
+        expect(new_post.errors[:content]).to include("can't be blank")
+      end  
+    end      
+  end      
 end
