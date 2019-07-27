1f0fb327f1a4e35b546e85e4fe5c39942aefe49b
TERVER AOSU <terveraosu@gmail.com>
2019-07-27 23:08:06 +0100

    made the tests checking for authenticated and unauthenticated user to pass

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,20 +2,39 @@ require 'rails_helper'
 require_relative '../support/devise'
 
 RSpec.describe UsersController do
-    login_user
+    describe "authenticated access" do
+        login_user
   
-    it "should have a current_user" do
-      expect(subject.current_user).to_not eq(nil)
-    end
-  
-    it "should get index" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-
-    it "should get show" do
-        get :show, params: { id: :user}
-        expect(response).to have_http_status(:success)
-    end
+        it "should have a current_user" do
+          expect(subject.current_user).to_not eq(nil)
+        end
       
+        it "should get index" do
+          get :index
+          expect(response).to have_http_status(:success)
+        end
+    
+        it "should get show" do
+            get :show, params: { id: :user}
+            expect(response).to have_http_status(:success)
+        end    
+    end  
+
+    describe "unauthenticated access" do
+  
+        it "should not have a current_user" do
+          expect(subject.current_user).to eq(nil)
+        end
+      
+        it "should not get index" do
+          get :index
+          expect(response).to redirect_to(new_user_session_path)
+        end
+    
+        it "should not get show" do
+            get :show, params: { id: :user}
+            expect(response).to redirect_to(new_user_session_path)
+        end    
+    end  
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,23 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe "create new post" do
-    before(:each) do
-      user = :user
-      @user = User.create(
-        name:  "poster",
-        email:      "poster@example.com",
-        password:   "123456",      
-      )
-    end
-    context "when post creation success" do
-    end
-
-    context "when post creation fails" do
-      it "is invalid without content" do
-        new_post = @user.posts.create(content: nil)
-        expect(new_post.errors[:content]).to include("can't be blank")
-      end  
-    end      
-  end      
 end
