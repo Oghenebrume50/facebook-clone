25b658a7f32973b82043e1729d57a39165816b21
TERVER AOSU <terveraosu@gmail.com>
2019-07-29 18:27:06 +0100

    made test for sending and accepting friend request to pass

--- spec/controllers/friendships_controller_spec.rb
+++ spec/controllers/friendships_controller_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe FriendshipsController, type: :controller do
+    describe "Friendships" do
+        context "when friend request sends" do
+          it "should increase friendships by one" do
+            user =  FactoryBot.create(:user)
+            friend = User.create(
+                name:  "Tester",
+                email:      "tester@example.com",
+                password:   "123456",      
+              )
+            params = {
+                user_id: user.id,
+                friend_id: friend.id,
+                confirmed: false
+            }
+            expect { 
+                user.friendships.create(params)
+             }.to change(Friendship, :count).by(1)
+          end  
+        end
 
+        context "when friend request is accepted" do
+            it "should increase friends by one" do
+              user =  FactoryBot.create(:user)
+              friend = User.create(
+                  name:  "Tester",
+                  email:      "tester@example.com",
+                  password:   "123456",      
+                )
+              params = {
+                  user_id: user.id,
+                  friend_id: friend.id,
+                  confirmed: false
+              }
+              user.friendships.create(params)
+              before_accept = user.friends.size
+              user.friendships.update(confirmed: true)
+              after_accept = user.friends.size
+              expect(before_accept).to_not eq(after_accept)
+            end  
+          end
+    end
 end
--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe LikesController, type: :controller do
-
+    describe "Like post" do
+        context "when post like succeeds" do
+          it "should increase likes on post" do
+            user =  FactoryBot.create(:user)
+            post = user.posts.create(
+                content: "This is the comment"
+            )
+            params = {
+                user_id: user.id
+            }
+            expect { 
+                post.likes.create(params)
+             }.to change(Like, :count).by(1)
+          end  
+        end
+    end
 end
