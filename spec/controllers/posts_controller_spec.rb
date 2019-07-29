793792d335e2567e207560f693f6ff0f9adeca01
Oghenebrume50 <raphlbrume@gmail.com>
2019-07-29 14:11:11 +0100

    removed test for current_user in post controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,10 +4,6 @@ require_relative '../support/devise'
 RSpec.describe PostsController, type: :controller  do
     login_user
   
-    it "should have a current_user" do
-      expect(subject.current_user).to_not eq(nil)
-    end
-  
     it "should get index" do
       get :index, params: { user_id: :user}
       expect(response).to have_http_status(:success)
