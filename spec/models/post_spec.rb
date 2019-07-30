87beac05c8118883f50b2496b7f21dcd00c2a4b3
Oghenebrume50 <raphlbrume@gmail.com>
2019-07-30 10:33:48 +0100

    added test for Post model validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  before do
+    @user = User.create(
+      name:  "poster",
+      email:      "poster@example.com",
+      password:   "123456",      
+    )
+  end
+  it "is invalid without content" do
+    new_post = @user.posts.create(content: nil)
+    expect(new_post.errors[:content]).to include("can't be blank")
+  end
+
+  it "is valid with content" do
+    expect {@user.posts.create(content: "I am pssing in content") }.to change(Post, :count).by(1)
+  end
 end
